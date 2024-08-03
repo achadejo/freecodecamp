6170674b5c74619252f84d7dc9d224a90df70661
achadejo <110856503+achadejo@users.noreply.github.com>
2024-08-03 23:56:49 +0100

    Updated script21.js

--- script21.js
+++ script21.js
@@ -3,11 +3,19 @@ const count = 8;
 const rows = [];
 
 function padRow(rowNumber, rowCount) {
-  return " " + character.repeat(rowNumber) + " ";
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-for (let i = 0; i < count; i = i + 1) {
-  rows.push(padRow(i + 1, count));
+// TODO: use a different type of loop
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+let continueLoop = false;
+let done = 0;
+
+while (continueLoop) {
+  done++;
 }
 
 let result = ""
